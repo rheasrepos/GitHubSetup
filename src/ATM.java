5c80968fe4ada26bdf4210b4d1791315e095cba5
rhea <rhea0866@gmail.com>
2021-08-27 09:12:23 -0700

    Initial attempt at assignment

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,24 @@
 
 public class ATM {
 
+	private String accountID;
+	private long total = 0;
+	
+	public ATM(String accountID) {
+		this.accountID = accountID;
+	}
+	
+	public ATM(){
+			accountID = "myIDABC";
+		
+	}
+
+	public void deposit(int ID, long deposit ) {
+		total += deposit;
+
+	}
+	
+	public long presentTotal() {
+		return total;
+	}
 }
